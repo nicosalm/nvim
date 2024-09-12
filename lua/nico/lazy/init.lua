4d58994f4b38056aaeee9959253d7c8007cd05dc
Nico <nico@salm.dev>
2024-09-12 10:03:31 -0500

    initial commit

---
+++ lua/nico/lazy/init.lua
@@ -0,0 +1,22 @@
+
+return {
+
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        opts = {
+
+        }
+    },
+
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+	},
+
+    -- "github/copilot.vim",
+	"wakatime/vim-wakatime",
+	"andweeb/presence.nvim",
+    "tpope/vim-commentary",
+    "alec-gibson/nvim-tetris",
+}
